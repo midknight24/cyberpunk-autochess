4c3d6487e35a88caffc6535ba0755c4bcd34ddaa
raynorc <raynorc@fnetlink.com>
2021-11-22 07:53:25 +0800

    init commit

---
+++ cyberpunk-autochess/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div>{{greeting}}</div>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    <div style="width: 50%;">
+      <el-row>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // },
+  data() {
+    return {
+      greeting: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.el-row {
+  margin-bottom: 20px;
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+</style>
---
+++ cyberpunk-autochess/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui';
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+Vue.use(ElementUI, {size: 'small'})
+
