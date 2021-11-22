75684944572ef0501643862848e2e5e5f6063e12
raynorc <raynorc@fnetlink.com>
2021-11-22 20:02:53 +0800

    finish grids

--- cyberpunk-autochess/src/App.vue
+++ cyberpunk-autochess/src/App.vue
@@ -1,25 +1,20 @@
 <template>
-  <div id="app">
-    <div>{{greeting}}</div>
-    <el-button type="primary" icon="el-icon-search">搜索</el-button>
-    <div style="width: 50%;">
-      <el-row>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-      </el-row>
+  <div id="app" style="height: 100%">
+    <div style="width: 50%; height: 100%">
+      <battle-grid style="height: 40%"></battle-grid>
+      <el-divider><i class="el-icon-close"></i></el-divider>
+      <battle-grid style="height: 40%"></battle-grid>
     </div>
   </div>
 </template>
 
 <script>
-
+import BattleGrid from './components/BattleGrid.vue'
 export default {
   name: 'App',
-  // components: {
-  //   HelloWorld
-  // },
+  components: {
+    BattleGrid
+  },
   data() {
     return {
       greeting: 'Hello World'
@@ -29,35 +24,16 @@ export default {
 </script>
 
 <style>
+body, html {
+    height: 100vh;
+    overflow: hidden;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
-.el-row {
-  margin-bottom: 20px;
-}
-.el-col {
-  border-radius: 4px;
-}
-.bg-purple-dark {
-  background: #99a9bf;
-}
-.bg-purple {
-  background: #d3dce6;
-}
-.bg-purple-light {
-  background: #e5e9f2;
-}
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
-.row-bg {
-  padding: 10px 0;
-  background-color: #f9fafc;
-}
+
 </style>
---
+++ cyberpunk-autochess/src/components/BattleGrid.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+      <el-row :gutter="10" class="battle-row">
+        <el-col :span="8" class="battle-slot"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="8" class="battle-slot"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="8" class="battle-slot"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row :gutter="10" class="battle-row">
+        <el-col :span="8" class="battle-slot"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="8" class="battle-slot"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="8" class="battle-slot"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row :gutter="10" class="battle-row">
+        <el-col :span="8" class="battle-slot"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="8" class="battle-slot"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="8" class="battle-slot"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BattleGrid',
+    prop: {
+        chessLists: Array
+    }
+    
+}
+</script>
+
+<style>
+.battle-row {
+    height: 35%;
+}
+.battle-slot {
+    height: 100%;
+}
+.el-row {
+  margin-bottom: 0px;
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+  height: 90%;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+</style>
